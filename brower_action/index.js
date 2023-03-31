d3ef585612da5c23037310d45ec55e0be678a453
park jaehyung <explainpark101@gmail.com>
2023-03-31 18:32:52 +0900

    기본설정 끝

--- brower_action/index.js
+++ brower_action/index.js
@@ -1,19 +1,24 @@
-
-
-
 window.addEventListener('DOMContentLoaded', () => {
   renderAll();
 })
 
+const storageGet = (key) => new Promise((resolve, reject)=>{
+  chrome.storage.local.get(key, (res)=>resolve(res));
+})
+
 const renderAll = async () => {
-  const PasswordBox = document.querySelector("input[name=passwordCode]");
-  await chrome.storage.local.get("EiE_ASSIST_activation",(result)=>{
-    let activationKey = result.EiE_ASSIST_activation;
-    if(activationKey != "undefined")
-    PasswordBox.value = activationKey;
-  });
-  
-  PasswordBox.addEventListener("change", (e)=>{
-    chrome.storage.local.set({"EiE_ASSIST_activation": PasswordBox.value});
-  });
+  const keys = ["institute-code", "password-code"];
+  const elements = keys.map(key=>document.querySelector(`[name='${key}']`));
+  elements.forEach(element=>{
+    element.addEventListener("input", ({target})=>{
+      const {value, name} = target;
+      let data = {};
+      data[name] = value;
+      chrome.storage.local.set(data);
+    });
+    storageGet(element.name)
+    .then(data=>{
+      element.value = data ?? '';
+    });
+  })
 }
